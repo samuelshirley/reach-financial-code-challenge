ceb6d0f75bd24e1d2044d9b664f9a9d17c7003a1
Samuel Shirley <37374497+samuelshirley@users.noreply.github.com>
2022-12-13 15:52:03 -0600

    change to one it block

--- API-Tests/get.spec.js
+++ API-Tests/get.spec.js
@@ -3,20 +3,12 @@ const { expect, assert } = require('chai');
 const config = require('../configs.json');
 
 describe('GET httpbin', () => {
-  it('should return a status of 200', async () => {
+  it('should confirm data and status in response', async () => {
     await axios({
       method: 'get',
       url: config.url + '/get',
     }).then((res) => {
       expect(res.status).to.equal(200);
-    });
-  });
-
-  it('should confirm data in response', async () => {
-    await axios({
-      method: 'get',
-      url: config.url + '/get',
-    }).then((res) => {
       assert.typeOf(res.data.args, 'object');
       assert.equal(res.data.origin, '136.62.164.172');
     });
